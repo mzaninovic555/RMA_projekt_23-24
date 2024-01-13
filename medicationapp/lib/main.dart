729d1d5eb26023e0ce6e35ece4c2ea07e2ffd990
Marko Zaninović <mzaninovic555@gmail.com>
2024-01-13 23:08:32 +0100

    init, added light/dark mode switching

--- medicationapp/lib/main.dart
+++ medicationapp/lib/main.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:medicationapp/pages/home_page.dart';
+import 'package:medicationapp/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MedicationApp(),
+    ),
+  );
+}
+
+class MedicationApp extends StatelessWidget {
+  const MedicationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
+  }
 }
---
+++ medicationapp/lib/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:medicationapp/components/box.dart';
+import 'package:medicationapp/components/button.dart';
+import 'package:medicationapp/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: MyBox(
+          color: Theme.of(context).colorScheme.primary,
+          child: MyButton(
+            color: Theme.of(context).colorScheme.secondary,
+            onTap: () {
+              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+            },
+          ),
+      )),
+    );
+  }
+}
