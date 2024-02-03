26068fd17236f853a9d95167ece1230ca623a5e9
Marko Zaninović <mzaninovic555@gmail.com>
2024-02-03 17:10:13 +0100

    Added login, register and sign out

---
+++ medicationapp/lib/pages/auth/auth_page.dart
@@ -0,0 +1,121 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:medicationapp/auth/auth.dart';
+
+class AuthWidget extends StatefulWidget {
+  bool isLogin;
+
+  AuthWidget(this.isLogin, {super.key});
+
+  @override
+  State<AuthWidget> createState() => _AuthWidgetState();
+}
+
+class _AuthWidgetState extends State<AuthWidget> {
+  String? errorMessage = '';
+
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+
+  Future<void> login() async {
+    try {
+      await Auth().signInWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Future<void> register() async {
+    try {
+      await Auth().createUserWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Text _title() {
+    return Text(widget.isLogin ? 'Login' : 'Register');
+  }
+
+  TextField _entryField(
+    String title,
+    TextEditingController controller,
+  ) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: title,
+      ),
+    );
+  }
+
+  Widget _errorMessage() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Text(
+        errorMessage ?? '',
+        style: TextStyle(
+          fontSize: 15.0,
+          color: Theme.of(context).colorScheme.primary,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  ElevatedButton _submitButton() {
+    return ElevatedButton(
+      onPressed: () async {
+        widget.isLogin ? await login() : await register();
+      },
+      child: const Text('Submit'),
+    );
+  }
+
+  TextButton _switchTypeButton() {
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          widget.isLogin = !widget.isLogin;
+        });
+      },
+      child: Text(widget.isLogin
+          ? 'Don\'t have an account? Register here.'
+          : 'Already have an account? Log in.'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: _title()),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _entryField('Email', _controllerEmail),
+            _entryField('Password', _controllerPassword),
+            _errorMessage(),
+            _submitButton(),
+            _switchTypeButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- medicationapp/lib/pages/settings/settings.dart
+++ medicationapp/lib/pages/settings/settings.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:medicationapp/pages/auth/auth_page.dart';
 import 'package:medicationapp/services/local_data_service.dart';
 import 'package:medicationapp/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
@@ -16,7 +17,7 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
-  final User? user = Auth().currentUser;
+  User? user = Auth().currentUser;
 
   static const TextStyle _settingsTextStyle = TextStyle(
     fontSize: 22.0,
@@ -72,17 +73,24 @@ class _SettingsState extends State<Settings> {
 
     if (isSignedIn) {
       return _settingsElement(Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(user!.email!),
+          Text(user!.email!, style: _settingsTextStyle),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () async {
+              await Auth().signOut();
+              setState(() {
+                user = Auth().currentUser;
+              });
+            },
             child: const Text('Sign out', style: _settingsTextStyle),
           ),
         ],
       ));
     }
 
-    return _settingsElement(height: 120,
+    return _settingsElement(
+      height: 120,
       Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -93,11 +101,27 @@ class _SettingsState extends State<Settings> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               ElevatedButton(
-                onPressed: () {},
-                child: const Text('Sign in', style: _settingsTextStyle),
+                onPressed: () async {
+                  await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AuthWidget(true)));
+                  setState(() {
+                    user = Auth().currentUser;
+                  });
+                },
+                child: const Text('Login', style: _settingsTextStyle),
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () async {
+                  await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AuthWidget(false)));
+                  setState(() {
+                    user = Auth().currentUser;
+                  });
+                },
                 child: const Text('Register', style: _settingsTextStyle),
               ),
             ],
